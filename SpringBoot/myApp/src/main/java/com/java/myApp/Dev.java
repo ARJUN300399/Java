f2d0bef8de51b32f5792ab7a62ea3d5cd706d896
Arjun <arj300399@gmail.com>
2024-07-15 21:20:27 +0530

    Dependency_Injection_SpringBoot

--- SpringBoot/myApp/src/main/java/com/java/myApp/Dev.java
+++ SpringBoot/myApp/src/main/java/com/java/myApp/Dev.java
@@ -1,2 +1,10 @@
-package com.java.myApp;public class Dev {
+package com.java.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    public  void build(){
+        System.out.println("Learning Spring boot");
+    }
 }

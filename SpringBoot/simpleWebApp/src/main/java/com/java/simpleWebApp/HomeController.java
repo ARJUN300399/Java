29703c4c5b40d5c459d9b90705a064d849f20423
Arjun <arj300399@gmail.com>
2024-07-20 23:22:10 +0530

    Simple WebApp SprijngBoot

--- SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/HomeController.java
+++ SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/HomeController.java
@@ -1,2 +1,12 @@
-package com.java.simpleWebApp;public class HomeController {
+package com.java.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String Greet(){
+        return "HelloJi....Sab Changa Si? !!!";
+    }
 }
--- SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/LoginController.java
+++ SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/LoginController.java
@@ -1,2 +1,13 @@
-package com.java.simpleWebApp;public class LoginController {
+package com.java.simpleWebApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @RequestMapping("/login")
+    public String Login(){
+        return "Login Page";
+    }
 }

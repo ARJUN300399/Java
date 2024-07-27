1a126469d94312322fd38263a1c6805a4e1bae50
Arjun <arj300399@gmail.com>
2024-07-27 20:15:44 +0530

    Rest API and JPA

--- SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/controller/HomeController.java
+++ SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.java.simpleWebApp;
+package com.java.simpleWebApp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/controller/LoginController.java
+++ SpringBoot/simpleWebApp/src/main/java/com/java/simpleWebApp/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.java.simpleWebApp;
+package com.java.simpleWebApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
